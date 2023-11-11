d809fdffdb4cb2c3edec6cd023efc066db62ce22
DMS <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-12 04:46:32 +0700

    first python code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-a = "namaku"
-b = "nugroho"
-c = "ganteng"
-abc = "namaku nugroho ganteng"
-print(abc)
-print(type(abc))
+"""
+semua sintaksi dasar terdiri dari :
+1.sekuensial : langkah berurutan
+2.Percabangan : Langkah melompat jika terpenuhi
+3.Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
+"""
