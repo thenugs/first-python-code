52eb97c84115f0eb25cbcb7d3674e773507e2a09
DMS <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-12 04:55:57 +0700

    first python code

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ semua sintaksi dasar terdiri dari :
 2.Percabangan : Langkah melompat jika terpenuhi
 3.Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
 """
+#sekunsial
+print('ibu berkata,"pergi ketoko"')
+print('Budi menjawab, "baik, apa yang harus saya lakukan di toko?"')
