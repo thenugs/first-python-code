363e1af50f1bc2ef66dd2437f06f4e3c19b7aede
thenugs <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-16 13:38:59 +0700

    my first python code

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-"""
-semua sintaksi dasar terdiri dari :
-1.sekuensial : langkah berurutan
-2.Percabangan : Langkah melompat jika terpenuhi
-3.Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
-"""
-#sekunsial
-print('ibu berkata,"pergi ketoko"')
-print('Budi menjawab, "baik, apa yang harus saya lakukan di toko?"')
+a = "namaku"
+b = "nugroho"
+print(a+b)
