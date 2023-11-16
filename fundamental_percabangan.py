5f562cca54063080e869089c7c37e2a705b5da3c
thenugs <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-16 13:32:24 +0700

    my first python code

---
+++ fundamental_percabangan.py
@@ -0,0 +1,6 @@
+"""
+semua sintaksis dasar bahasa pemrograman terdiri dari :
+1. Sekuel : langkah berurutan
+2. Percabangan : langkah melomat jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
+"""
