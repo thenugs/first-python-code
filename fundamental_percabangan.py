9bbbd4c51b3e52e49d5c1eae2a2e570b0496b986
DMS <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-17 17:24:31 +0700

    first python code

--- fundamental_percabangan.py
+++ fundamental_percabangan.py
@@ -6,8 +6,8 @@ print("maka budi berangkat ke toko")
 print("dan mulai berbelanja")
 
 # Percabangan
-jumlah_botol_susu = 1
-if jumlah_botol_susu > 3 :
+jumlah_botol_susu = 3
+if jumlah_botol_susu >= 3 :
     print("budi mengecek harganya, dan ternyata uangnya cukup")
     print("budi membeli 1 botol susu")
 else :
