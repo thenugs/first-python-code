cbdc4277c118df3d2a5bf9b6a5e5f4a964966ad8
DMS <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-17 20:57:54 +0700

    first python code

--- latihan_if_else.py
+++ latihan_if_else.py
@@ -1,1 +1,9 @@
-# latihan if else
+''' latihan if else
+ if else ini, jika di materi matematika sama dengan logika matematika, ada konjungsi, implikasi, biimplikasi
+'''
+p = 0.5
+q = 21.5
+
+if p>q:
+    print("p lebih besar dari q")
+else:print("q lebih besar dari p")
