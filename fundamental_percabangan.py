9054ebe76461549888705e0b9e8c492dbb673f62
thenugs <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-17 14:15:23 +0700

    my first python code

--- fundamental_percabangan.py
+++ fundamental_percabangan.py
@@ -1,6 +1,17 @@
-"""
-semua sintaksis dasar bahasa pemrograman terdiri dari :
-1. Sekuel : langkah berurutan
-2. Percabangan : langkah melomat jika kondisi terpenuhi
-3. Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
-"""
+# sekuensial
+print('ibu berkata, "pergi ke toko"')
+print('budi berkata, "baik apa yang harus saya lakukan di toko?"')
+print('ibu menjawab, "beli satu botol susu"')
+print("maka budi berangkat ke toko")
+print("dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 0
+if jumlah_botol_susu > 3 :
+    print("budi mengecek harganya, dan ternyata uangnya cukup")
+    print("budi membeli 1 botol susu")
+else :
+    print("budi tidak jadi membeli 1 botol susu")
+
+print("budi pulang kerumah")
+print("menyampaikan kepada ibu")
