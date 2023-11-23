77685a11e0894de540ff15f25250a38f5fbc64ae
thenugs <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-23 15:37:39 +0700

    bober

---
+++ List,Tuple,Dictionary.py
@@ -0,0 +1,20 @@
+# List, Tuple data is tipe data yang bisa menampung banyak nilai data dalam satu variable
+
+# belajar List
+# list merupakan kumpulan data terurut (ordered sequence)
+# dapat dirubah
+# boleh duplikat
+# List dibuat dengan kurung siku dan setiap elemen dipisah dengan tanda koma
+# setiap elemen memiliki no urut (index) yang dimulai dari 0,1,2,3 dst
+# yuk mulai
+
+x = [8,9,"OTKP","ATPH",True]
+print(x) # hasilnya [8, 9, 'OTKP', 'ATPH', True]
+print(x[2]) #(x[2]) maksudnya adalah mencetak index ke 2 yaitu hasilnya OTKP
+print(x[-5]) # (x[-5]) maksudnya adalah mencetak tapi mulai dari belakang
+
+# Fungsi fungsi pada list
+x = [8,9,"OTKP","ATPH",True]
+
+x =(len(x)) # hasilnya = 8 , (len(x)) ini untuk mendapatkan panjang list yang ada di variabel x
+x [2]= semarang
