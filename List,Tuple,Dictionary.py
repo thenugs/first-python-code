a7b808a28d95de16eab6020db03e511c66d6222a
thenugs <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-24 13:44:48 +0700

    bober

--- List,Tuple,Dictionary.py
+++ List,Tuple,Dictionary.py
@@ -1,5 +1,4 @@
 # List, Tuple data is tipe data yang bisa menampung banyak nilai data dalam satu variable
-
 # belajar List
 # list merupakan kumpulan data terurut (ordered sequence)
 # dapat dirubah
@@ -13,11 +12,38 @@ print(x) # hasilnya [8, 9, 'OTKP', 'ATPH', True]
 print(x[2]) #(x[2]) maksudnya adalah mencetak index ke 2 yaitu hasilnya OTKP
 print(x[-5]) # (x[-5]) maksudnya adalah mencetak tapi mulai dari belakang
 
+
 # Fungsi fungsi pada list
 x = [8,9,"OTKP","ATPH",True]
 
-print(x) =(len(x)) # hasilnya = 8 , (len(x)) ini untuk mendapatkan panjang list yang ada di variabel x
-x[2]= "semarang"
+# mencari panjang list
+print(len(x)) # hasilnya = 8 , (len(x)) ini untuk mendapatkan panjang list yang ada di variabel x
 print(x)
 
-# masih eror besok belajar lagi ya gaes
+# mengubah nilai pada index ke
+x[2]= "tokyo"
+print(x) # [8, 9, 'tokyo', 'ATPH', True]
+x[3]= 25
+print(x) # [8, 9, 'tokyo', 25, True]
+
+# menambah element pada index terakhir
+x.append("merah")
+print(x) # [8, 9, 'tokyo', 25, True, 'merah']
+
+# menambah element pada index ke-1
+x.insert(1,"tomat")
+print(x) #[8, 'tomat', 9, 'tokyo', 25, True, 'merah']
+
+# menghapus element tertentu
+x.remove("tomat")
+print(x) #[8, 9, 'tokyo', 25, True, 'merah']
+
+# menghapus element pada index ke 0
+x.pop(0)
+print(x) #[9, 'tokyo', 25, True, 'merah']
+
+x.clear()
+print(x)
+
+
+
