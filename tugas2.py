98c00cb39fab8e86eecdb24de5c6b7fa94afc429
thenugs <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-27 15:37:21 +0700

    if else elif

---
+++ tugas2.py
@@ -0,0 +1,13 @@
+# deret angka 1-30
+# tiap kelipatan angka 3 muncul dang
+# tiap kelipatan 5 muncul dut
+# tiap kelipatan 3 dan 5 muncul dangdut
+
+for i in range(1,30):
+    print(i)
+    if i%3 == 0:
+        print("dang")
+    if i%5 == 0:
+        print("dut")
+
+# masih eror (yuk belajar lagi)
