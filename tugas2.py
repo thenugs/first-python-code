ba3622136c03d0e2aba759ab0a9eb67c11552c5b
DMS <nugrohosetiadi62@guru.smk.belajar.id>
2023-11-28 21:53:25 +0700

    first python code

--- tugas2.py
+++ tugas2.py
@@ -3,6 +3,7 @@
 # tiap kelipatan 5 muncul dut
 # tiap kelipatan 3 dan 5 muncul dangdut
 
+"""
 for i in range(1,31):
     if i%3 == 0 and i%5 ==0:
         print("dangdut")
@@ -12,6 +13,30 @@ for i in range(1,31):
         print("dut")
     else:
         print(i)
+"""
+# belajar elif else
+# nilai A = 100-90
+# nilai B = 80-89
+# nilai C = 70-79
+# nilai D = 60-69
+# nilai E = 0-59
+
+nilai = int(input("masukan nilai :"))
+if (nilai==100):
+    print("nilai anda A")
+elif (nilai>=90):
+    print("nilai anda A")
+elif (nilai>=80):
+    print("nilai anda B")
+elif (nilai>=70):
+    print("nilai anda C")
+elif (nilai>=60):
+    print("nilai anda D")
+elif (nilai>=50):
+    print("nilai anda E")
+elif (nilai>=0):
+    print("selamat anda terdaftar dalam kategori parah sekali")
+    print("anda harus bersabar bahwa hidup ini adalah ujian")
 
 
 
